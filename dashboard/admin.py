ff7f8b72121fa0438e1fb578041df283bbdf951e
mudassaralimosu <mudassar786mosu@gmail.com>
2023-01-16 21:08:52 +0530

    inventory project

---
+++ dashboard/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Product, Order
+from django.contrib.auth.models import Group
+
+
+admin.site.site_header = 'InventoryMosu Dashboard'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category','quantity')
+    list_filter = ['category']
+
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
+
+# With this code Group from the admin will be deleted
+# admin.site.unregister(Group)
