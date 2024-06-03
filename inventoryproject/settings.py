fe2ad4bde5c423f76886c8e719009faca160b430
Mosu <71853471+Mudassaralimosu@users.noreply.github.com>
2024-06-03 07:44:16 +0530

    Update settings.py

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -141,7 +141,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mudassar786work@gmail.com'
-EMAIL_HOST_PASSWORD = 'llclattiiddkswgl'
+EMAIL_HOST_PASSWORD = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
